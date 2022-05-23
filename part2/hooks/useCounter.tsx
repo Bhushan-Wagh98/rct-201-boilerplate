6675392e703bfbdaca17b0c57a4adabec1c32bd5
Bhushan Wagh <wagh.bhushan.998@gmail.com>
2022-05-23 17:07:08 +0530

    evaluation

--- part2/hooks/useCounter.tsx
+++ part2/hooks/useCounter.tsx
@@ -1,1 +1,24 @@
-export const useCounter = (init) => {};
+import { useState } from "react";
+
+export const useCounter = (init: number) => {
+  const [value, setValue] = useState(init);
+  const inc = (n?: number) => {
+    if (n) {
+      setValue(value + n);
+    } else {
+      setValue(value + 1);
+    }
+  };
+  const dec = (n?: number) => {
+    if (n) {
+      setValue(value - n);
+    } else {
+      setValue(value - 1);
+    }
+  };
+  const set = (n: number) => {
+    setValue(n);
+  };
+
+  return { value, inc, dec, set };
+};
--- part2/hooks/useList.tsx
+++ part2/hooks/useList.tsx
@@ -1,1 +1,27 @@
-export const useList = (arr) => {};
+import { useState } from "react";
+
+export const useList = (arr: number[]) => {
+  const [list, setList] = useState<number[]>(arr);
+  const push = (value: number): void => {
+    setList([...list, value]);
+  };
+
+  const pop = (): void => {
+    list.pop();
+    setList([...list]);
+  };
+
+  const clear = (): void => {
+    setList([]);
+  };
+
+  const reset = (): void => {
+    setList([1, 2, 3]);
+  };
+
+  const map = () => {
+    setList(list.map((el) => el * 2));
+  };
+
+  return [list, { push, pop, clear, reset, map }];
+};
